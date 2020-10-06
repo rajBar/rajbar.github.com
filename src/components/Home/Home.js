bfc9e7607145ee9ed2806b037ad93b90e8f78c8b
Raj Bariah <raj.uni@outlook.com>
2020-10-06 09:01:06 +0100

    styling updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,16 +7,18 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            prefix: "r",
+            r: "r",
+            prefix: "",
             suffix: "",
             middle: ".",
             link: "http://raj.Bar",
         }
     }
 
-    changeText = (prefix, middle, suffix, link) => {
+    changeText = (prefix, r, middle, suffix, link) => {
         this.setState({
             ...this.state,
+            r: r,
             prefix: prefix,
             suffix: suffix,
             middle: middle,
@@ -28,43 +30,67 @@ class Home extends Component {
         const prefix = this.state.prefix;
         const suffix = this.state.suffix;
         const middle = this.state.middle;
+        const r = this.state.r;
         const link = this.state.link;
+        const other = "#484848"
 
         return (
             <div style={{ marginTop: "50px" }}>
-                <a href={link} style={{ textDecoration: "none", color: "black"}}>
+                <a href={link} target="_blank" style={{ textDecoration: "none", color: "black"}}>
                     <section style={{ fontSize: 90 }}>
                         <TextTransition
                             text={prefix}
                             springConfig={presets.wobbly}
                             inline
                             overflow
+                            style={{ color: other }}
                         />
-                        aj
+                        <TextTransition
+                            text={r}
+                            springConfig={presets.wobbly}
+                            inline
+                            overflow
+                            style={{ color: "black", fontWeight: "bold" }}
+                        />
+                        <TextTransition
+                            text="aj"
+                            springConfig={presets.wobbly}
+                            inline
+                            overflow
+                            style={{ color: "black", fontWeight: "bold" }}
+                        />
                         <TextTransition
                             text={middle}
                             springConfig={presets.wobbly}
                             inline
                             overflow
+                            style={{ color: other }}
                         />
-                        Bar
+                        <TextTransition
+                            text="Bar"
+                            springConfig={presets.wobbly}
+                            inline
+                            overflow
+                            style={{ color: "black", fontWeight: "bold" }}
+                        />
                         <TextTransition
                             text={suffix}
                             springConfig={presets.wobbly}
                             inline
                             overflow
+                            style={{ color: other }}
                         />
                     </section>
                 </a>
                 <div style={{ marginTop: "80px" }}>
-                    <p onClick={() => this.changeText("r", "", "", "https://github.com/rajBar/")}>GitHub</p>
-                    <p onClick={() => this.changeText("Gulraj (R", ")‎‎‎ ‎", "iah", "https://uk.linkedin.com/in/gulrajbariah")}>LinkedIn</p>
-                    <p onClick={() => this.changeText("@R", "", "iah", "https://www.instagram.com/rajbariah/?hl=en")}>Instagram</p>
-                    <p onClick={() => this.changeText("R", "‎‎‎ ‎", "iah", "https://www.strava.com/athletes/59236473")}>Strava Account</p>
-                    <p onClick={() => this.changeText("r", "", ".github.io/strava-stats", "https://rajbar.github.io/strava-stats/")}>Strava Stats</p>
-                    <p onClick={() => this.changeText("me@r", ".", "‎‎‎ ‎(Not Active)", "mailto:me@raj.Bar")}>E-mail</p>
-                    <p onClick={() => this.changeText("r", "", ".github.io/movie-list", "https://rajbar.github.io/movie-list/")}>Movie List</p>
-                    <p onClick={() => this.changeText("r", "", ".github.io/blogs", "https://rajbar.github.io/blogs/")}>Blog</p>
+                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r","", "", "https://github.com/rajBar/")}>GitHub</p>
+                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("Gulraj (", "R", ")‎‎‎ ‎", "iah", "https://uk.linkedin.com/in/gulrajbariah")}>LinkedIn</p>
+                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("@", "R", "", "iah", "https://www.instagram.com/rajbariah/?hl=en")}>Instagram</p>
+                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "R", "‎‎‎ ‎", "iah", "https://www.strava.com/athletes/59236473")}>Strava Account</p>
+                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r", "", ".github.io/strava-stats", "https://rajbar.github.io/strava-stats/")}>Strava Stats</p>
+                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r", "", "iah@gmail.com‎‎‎", "mailto:me@rajbariah@gmail.com")}>E-mail</p>
+                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r", "", ".github.io/movie-list", "https://rajbar.github.io/movie-list/")}>Movie List</p>
+                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r", "", ".github.io/blogs", "https://rajbar.github.io/blogs/")}>Blog</p>
                 </div>
             </div>
         )
