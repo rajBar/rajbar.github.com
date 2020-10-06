f97f475bfa03771794d2c4f688204ee47a0f7503
Raj Bariah <raj.uni@outlook.com>
2020-10-06 15:05:55 +0100

    small updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,7 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            selected: "",
             r: "r",
             prefix: "",
             suffix: "",
@@ -15,9 +16,10 @@ class Home extends Component {
         }
     }
 
-    changeText = (prefix, r, middle, suffix, link) => {
+    changeText = (buttonSelected, prefix, r, middle, suffix, link) => {
         this.setState({
             ...this.state,
+            selected: buttonSelected,
             r: r,
             prefix: prefix,
             suffix: suffix,
@@ -32,7 +34,6 @@ class Home extends Component {
         const middle = this.state.middle;
         const r = this.state.r;
         const link = this.state.link;
-        const other = "#484848"
 
         return (
             <div style={{ marginTop: "50px" }}>
@@ -43,21 +44,21 @@ class Home extends Component {
                             springConfig={presets.wobbly}
                             inline
                             overflow
-                            style={{ color: other }}
+                            className="other-text"
                         />
                         <TextTransition
                             text={r}
                             springConfig={presets.wobbly}
                             inline
                             overflow
-                            style={{ color: "black", fontWeight: "bold" }}
+                            className="main-text"
                         />
                         <TextTransition
                             text="aj"
                             springConfig={presets.wobbly}
                             inline
                             overflow
-                            style={{ color: "black", fontWeight: "bold" }}
+                            className="main-text"
                         />
                         <TextTransition
                             text={middle}
@@ -65,14 +66,14 @@ class Home extends Component {
                             inline
                             overflow
                             delay={100}
-                            style={{ color: other }}
+                            className="other-text"
                         />
                         <TextTransition
                             text="Bar"
                             springConfig={presets.wobbly}
                             inline
                             overflow
-                            style={{ color: "black", fontWeight: "bold" }}
+                            className="main-text"
                         />
                         <TextTransition
                             text={suffix}
@@ -80,19 +81,19 @@ class Home extends Component {
                             inline
                             overflow
                             delay={200}
-                            style={{ color: other }}
+                            className="other-text"
                         />
                     </section>
                 </a>
                 <div style={{ marginTop: "80px" }}>
-                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r","", "", "https://github.com/rajBar/")}>GitHub</p>
-                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("Gulraj (", "R", ")‎‎‎ ‎", "iah", "https://uk.linkedin.com/in/gulrajbariah")}>LinkedIn</p>
-                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("@", "R", "", "iah", "https://www.instagram.com/rajbariah/?hl=en")}>Instagram</p>
-                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "R", "‎‎‎ ‎", "iah", "https://www.strava.com/athletes/59236473")}>Strava Account</p>
-                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r", "", ".github.io/strava-stats", "https://rajbar.github.io/strava-stats/")}>Strava Stats</p>
-                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r", "", "iah@gmail.com‎‎‎", "mailto:me@rajbariah@gmail.com")}>E-mail</p>
-                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r", "", ".github.io/movie-list", "https://rajbar.github.io/movie-list/")}>Movie List</p>
-                    <p style={{ cursor: "pointer" }} onClick={() => this.changeText("", "r", "", ".github.io/blogs", "https://rajbar.github.io/blogs/")}>Blog</p>
+                    <p className={this.state.selected === "github" ? "link-button-selected" : "link-button"} onClick={() => this.changeText("github", "", "r","", "", "https://github.com/rajBar/")}>GitHub</p>
+                    <p className={this.state.selected === "linkedin" ? "link-button-selected" : "link-button"} onClick={() => this.changeText("linkedin", "Gulraj (", "R", ")‎‎‎ ‎", "iah", "https://uk.linkedin.com/in/gulrajbariah")}>LinkedIn</p>
+                    <p className={this.state.selected === "instagram" ? "link-button-selected" : "link-button"} onClick={() => this.changeText("instagram", "@", "R", "", "iah", "https://www.instagram.com/rajbariah/?hl=en")}>Instagram</p>
+                    <p className={this.state.selected === "strava" ? "link-button-selected" : "link-button"} onClick={() => this.changeText("strava", "", "R", "‎‎‎ ‎", "iah", "https://www.strava.com/athletes/59236473")}>Strava Account</p>
+                    <p className={this.state.selected === "strava-stats" ? "link-button-selected" : "link-button"} onClick={() => this.changeText("strava-stats", "", "r", "", ".github.io/strava-stats", "https://rajbar.github.io/strava-stats/")}>Strava Stats</p>
+                    <p className={this.state.selected === "email" ? "link-button-selected" : "link-button"} onClick={() => this.changeText("email", "", "r", "", "iah@gmail.com‎‎‎", "mailto:me@rajbariah@gmail.com")}>E-mail</p>
+                    <p className={this.state.selected === "movie-list" ? "link-button-selected" : "link-button"} onClick={() => this.changeText("movie-list", "", "r", "", ".github.io/movie-list", "https://rajbar.github.io/movie-list/")}>Movie List</p>
+                    <p className={this.state.selected === "blogs" ? "link-button-selected" : "link-button"} onClick={() => this.changeText("blogs", "", "r", "", ".github.io/blogs", "https://rajbar.github.io/blogs/")}>Blog</p>
                 </div>
             </div>
         )
