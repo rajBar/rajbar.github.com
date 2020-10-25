3f37fc3e78a5d6860b6543f524d3b793b794c492
Raj Bariah <raj.uni@outlook.com>
2020-10-25 21:34:40 +0000

    Updates

---
+++ precache-manifest.a7cc60fa75c1254ef24c1aa8ab61ed37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1a7589b0f40905ac27c06301b3ac88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caace97ad3d09de47c6b",
+    "url": "/static/css/main.0843ae42.chunk.css"
+  },
+  {
+    "revision": "ee4dcff65d9ccb3554ce",
+    "url": "/static/js/2.595dd15f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.595dd15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caace97ad3d09de47c6b",
+    "url": "/static/js/main.0132d2ba.chunk.js"
+  },
+  {
+    "revision": "cd54e75a5be495ba95c6",
+    "url": "/static/js/runtime-main.19a3ffa3.js"
+  },
+  {
+    "revision": "c5ddecc677d10dd4ae1a9216bf75dff7",
+    "url": "/static/media/Instagram.c5ddecc6.png"
+  },
+  {
+    "revision": "e7d19b9bf6a1fa353d3e521a997c6edb",
+    "url": "/static/media/Strava.e7d19b9b.png"
+  }
+]);
